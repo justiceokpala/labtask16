ab7569ba8a900fc0735d55c84c49313a56c5316c
Justice Okpala <justicecharles497@gmail.com>
2020-11-12 23:45:14 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DelegateApp
+{
+    public delegate string SpaceDel(string str);
+    public delegate string ReverseDel(string str);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strMyDel = new TestDelegate();
+            SpaceDel spaceDel = strMyDel.Space;
+            ReverseDel reverseDel = strMyDel.Reverse;
+            Console.WriteLine(spaceDel("Pamela"));
+            Console.WriteLine(reverseDel("Samuel"));
+        }
+    }
+}
